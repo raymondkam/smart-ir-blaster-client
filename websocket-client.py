1172eaaa7b779973ae93d597f0ae5772825a1074
Raymond Kam <raymondhmkam@gmail.com>
2017-12-11 02:37:27 -0500

    Fixed extra space in HDMI1 definition, added default num_times of 1

--- websocket-client.py
+++ websocket-client.py
@@ -12,13 +12,11 @@ with open('./config/websocket.json') as json_data:
 
 pingInterval = 30
 samsung_remote_ir_ids = ["KEY_MUTE", "KEY_VOLUMEUP", "KEY_VOLUMEDOWN"]
-samsung_discrete_ir_ids = ["POWER_ON", "POWER_OFF", "SOURCE_TV"," SOURCE_HDMI1", "SOURCE_HDMI2"]
+samsung_discrete_ir_ids = ["POWER_ON", "POWER_OFF", "SOURCE_TV", "SOURCE_HDMI1","SOURCE_HDMI2"]
 
 def ir_send(remote_name, ir_id, delay, num_times):
     time.sleep(delay)
     print("\nIR: Sending IR command {} on {}").format(ir_id, remote_name)
-    if num_times < 1:
-        num_times = 1
     error = subprocess.call(["irsend", "-#", str(num_times), "SEND_ONCE", remote_name, ir_id])
     if error:
         print("IR: sending IR command {} {} times on {} failed").format(ir_id, num_times, remote_name)
@@ -43,7 +41,7 @@ def on_message(ws, message):
         for command in commands:
             ir_id = command["id"]
             delay = float(command["delay"])
-            num_times = int(command["numTimes"])
+            num_times = int(command.get("numTimes", "1"))
             if ir_id in samsung_remote_ir_ids:
                 ir_send("SAMSUNG_REMOTE", ir_id, delay, num_times)
             elif ir_id in samsung_discrete_ir_ids:
