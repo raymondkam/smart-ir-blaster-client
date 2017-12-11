56f18ca2b8a740b5024480eeb2e5e259c56119a3
Raymond Kam <raymondhmkam@gmail.com>
2017-12-11 01:51:52 -0500

    Added ability to send # times for a specific command. Added ping function to keep websocket alive. Pings every 15 seconds.

--- websocket-client.py
+++ websocket-client.py
@@ -10,17 +10,20 @@ with open('./config/websocket.json') as json_data:
     token = websocketJSON["token"]
     server_address = websocketJSON["server_address"]
 
+pingInterval = 15
 samsung_remote_ir_ids = ["KEY_MUTE", "KEY_VOLUMEUP", "KEY_VOLUMEDOWN"]
 samsung_discrete_ir_ids = ["POWER_ON", "POWER_OFF", "SOURCE_TV"," SOURCE_HDMI1", "SOURCE_HDMI2"]
 
-def ir_send(remote_name, ir_id, delay):
+def ir_send(remote_name, ir_id, delay, num_times):
     time.sleep(delay)
     print("\nIR: Sending IR command {} on {}").format(ir_id, remote_name)
-    error = subprocess.call(["irsend", "SEND_ONCE", remote_name, ir_id])
+    if num_times < 1:
+        num_times = 1
+    error = subprocess.call(["irsend", "-#", num_times, "SEND_ONCE", remote_name, ir_id])
     if error:
-        print("IR: sending IR command {} on {} failed").format(ir_id, remote_name)
+        print("IR: sending IR command {} {} times on {} failed").format(ir_id, num_times, remote_name)
     else:
-        print("IR: sending IR command {} on {} succeeded").format(ir_id, remote_name)
+        print("IR: sending IR command {} {} times on {} succeeded").format(ir_id, num_times, remote_name)
 
 def on_message(ws, message):
     message_json = json.loads(message)
@@ -40,13 +43,15 @@ def on_message(ws, message):
         for command in commands:
             ir_id = command["id"]
             delay = float(command["delay"])
+            num_times = int(command["numTimes"])
             if ir_id in samsung_remote_ir_ids:
-                ir_send("SAMSUNG_REMOTE", ir_id, delay)
+                ir_send("SAMSUNG_REMOTE", ir_id, delay, num_times)
             elif ir_id in samsung_discrete_ir_ids:
-                ir_send("SAMSUNG_DISCRETE", ir_id, delay)
+                ir_send("SAMSUNG_DISCRETE", ir_id, delay, num_times)
             else:
                 print("Unrecognized IR id")
-
+    elif message_json["type"] == "pong":
+        print("WS: received pong")
 def on_error(ws, error):
     print(error)
 
@@ -54,12 +59,18 @@ def on_close(ws):
     print("### closed ###")
 
 def on_open(ws):
+    def ping(*args):
+        while True:
+            time.sleep(pingInterval)
+            print("WS: sending ping")
+            ws.send(json.dumps({"token": token, "type": "ping"}))
+    thread.start_new_thread(ping, ())
+
     print("### websocket opened ###")
     ws.send(json.dumps({"token": token, "type": "auth"}))
 
 if __name__ == "__main__":
-    websocket.enableTrace(True)
-    ws = websocket.WebSocketApp("wss://" + server_address,
+    ws = websocket.WebSocketApp("ws://" + server_address,
                               on_message = on_message,
                               on_error = on_error,
                               on_close = on_close)
