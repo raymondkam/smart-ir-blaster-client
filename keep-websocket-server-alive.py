a71cd98e5c2628341e0c67eb60f01efbac496025
Raymond Kam <raymondhmkam@gmail.com>
2017-12-11 22:33:18 -0500

    Separate script to keep websocket server alive

---
+++ keep-websocket-server-alive.py
@@ -0,0 +1,20 @@
+import subprocess
+import thread
+import time
+import json
+
+with open('./config/websocket.json') as json_data:
+    websocketJSON = json.load(json_data)
+    health_check_server_address = websocketJSON["health_check_server_address"]
+
+health_check_interval = 60
+
+def keep_websocket_server_alive():
+    while True:
+        print("\n### Checking websocket server health ###")
+        subprocess.call(["curl", "-X", "GET", health_check_server_address])
+        time.sleep(health_check_interval)
+
+if __name__ == "__main__":
+    keep_websocket_server_alive()
+
